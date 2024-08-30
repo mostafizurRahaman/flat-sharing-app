9d463747959a7875366935ebf3ca39962ad34ffa
mostafizurRahaman <mostafizurrahaman0401@gmail.com>
2024-08-30 23:49:44 +0600

    feat: Format  Global Error Handler Middleware for all errors

--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -1,34 +1,34 @@
-
 export interface IJwtPayload {
   id: string;
   email: string;
 }
 
-export interface IOptions { 
-  limit?: number; 
+export interface IOptions {
+  limit?: number;
   page?: number;
-  sortBy?: string; 
-  sortOrder?:string
+  sortBy?: string;
+  sortOrder?: string;
 }
 
-
-export interface IPaginationOptions { 
-
-  limit: number; 
-  page: number; 
+export interface IPaginationOptions {
+  limit: number;
+  page: number;
   sortBy: string;
-  sortOrder: "asc" | "desc"
-  skip: number
+  sortOrder: "asc" | "desc";
+  skip: number;
 }
 
-
-interface IIssues {
+export interface IIssues {
   field: string;
   message: string;
 }
 
-
-
 export interface IErrorDetails {
   issues: IIssues[];
+}
+
+export interface IErrorReturnType {
+  statusCode: number;
+  message: string;
+  errDetails: IErrorDetails | Error;
 }
--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -4,6 +4,11 @@ import { ZodError, ZodIssue } from "zod";
 import handleZodError from "../errors/handleZodError";
 import { IErrorDetails } from "../interfaces";
 import configs from "../configs";
+import { Prisma } from "@prisma/client";
+import handlePrismaClientKnownRequestError from "../errors/handlePrismaClientKnownError";
+import handlePrismaClientUnKnownError from "../errors/handlePrismaClientUnKnownError";
+import handlePCInitializationError from "../errors/handlePCInitializationError";
+import AppError from "../errors/AppError";
 
 const globalErrorHandler = (
   err: Error,
@@ -11,7 +16,7 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  let message: string = "Something Went Wrong";
+  let message: string = err.message || "Something Went Wrong";
   let errDetails: IErrorDetails | Error = err;
   let statusCode: number = httpStatus.INTERNAL_SERVER_ERROR;
 
@@ -22,6 +27,38 @@ const globalErrorHandler = (
     message = simplifiedErr.message;
     errDetails = simplifiedErr.errDetails;
     statusCode = simplifiedErr.statusCode;
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    const simplifiedErr = handlePrismaClientKnownRequestError(err);
+
+    //  Set Values
+    message = simplifiedErr.message;
+    errDetails = simplifiedErr.errDetails;
+    statusCode = simplifiedErr.statusCode;
+  } else if (err instanceof Prisma.PrismaClientInitializationError) {
+    const simplifiedErr = handlePCInitializationError(err);
+    console.dir({ err }, { depth: Infinity });
+
+    //  Set Values
+    message = simplifiedErr.message;
+    errDetails = simplifiedErr.errDetails;
+    statusCode = simplifiedErr.statusCode;
+  } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    const simplifiedErr = handlePrismaClientUnKnownError(err);
+
+    //  Set Values
+    message = simplifiedErr.message;
+    errDetails = simplifiedErr.errDetails;
+    statusCode = simplifiedErr.statusCode;
+  } else if (err instanceof Prisma.PrismaClientValidationError) {
+    const simplifiedErr = handlePrismaClientUnKnownError(err);
+
+    //  Set Values
+    message = simplifiedErr.message;
+    errDetails = simplifiedErr.errDetails;
+    statusCode = simplifiedErr.statusCode;
+  } else if (err instanceof AppError){
+    message = err.message; 
+    statusCode = err.statusCode;     
   }
 
   res.status(statusCode).json({
